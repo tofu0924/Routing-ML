ebf68af6b5e33cca6cd3f5f5794959f3f37fa9b7
tofu0924 <34615620+tofu0924@users.noreply.github.com>
2021-02-04 00:19:49 +0900

    Create Pad.py

---
+++ Pad.py
@@ -0,0 +1,10 @@
+class Point:
+  def __init__(self, x, y):
+    self.x = x;
+    self.y = y;
+  def getPoint(self):
+    return (self.x, self.y)
+  def
+class Pad(){
+  def __init__(self, line1point1x, line1point1x
+}
