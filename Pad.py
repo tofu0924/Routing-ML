705b3fed9604ec84bd2d7ac3c93a6396b52c577d
tofu0924 <in_no@naver.com>
2021-02-04 00:51:20 +0900

    concretating pad class ~

--- Pad.py
+++ Pad.py
@@ -1,10 +1,27 @@
-class Point:
-  def __init__(self, x, y):
-    self.x = x;
-    self.y = y;
-  def getPoint(self):
-    return (self.x, self.y)
-  def
-class Pad(){
-  def __init__(self, line1point1x, line1point1x
-}
+from abc import *
+class move(metaclass=ABCMeta):
+    @abstractproperty
+    pad = 'pad'
+    @abstractmethod
+    def move(self):
+        pass
+
+class moveInAnArc(move):
+    def __init__(self, pad):
+        self.pad = pad
+    def move(self, theta):
+        curline = self.pad.getCurLine()
+        p1 = curline.p1
+        p2 = curline.p2
+        
+
+
+
+
+class Pad():
+    def __init__(self, startline, endline):
+        self.curLine = startline
+        self.endLine = endline2
+
+    def getCurLine(self):
+        return self.curLine
